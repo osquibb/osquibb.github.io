7b4c777c85eca0d40a4f7b378978756117cb53e5
osquibb <osquibb@gmail.com>
2019-03-04 13:32:28 -0500

    updates

--- Test Sites/Test Site 1/js/script.js
+++ Test Sites/Test Site 1/js/script.js
@@ -1,24 +1,35 @@
-$(function() {
+// TODO: Create a JSON file that contains image urls, titles & descriptions
+// for the tiles.  Then populate the tiles with the buildAndShowImageTiles function.
 
-buildAndShowImageTiles("#main-content", 10);
+(function (window) {
 
-});
+  var imageInfo = {};
 
-function buildAndShowImageTiles (selector, numberOfTiles) {
+  document.addEventListener("DOMContentLoaded", function (event) {
 
-  $ajaxUtils.sendGetRequest("snippets/image-snippet.html", function (response)
-  {
-    var finalHTML = '<section class="row">';
-    for (var i = 0; i < numberOfTiles; i++) {
-      finalHTML += response;
-    };
-    finalHTML += '</section>';
-    insertHTML(selector, finalHTML);
-    }, false);
-}
+    buildAndShowImageTiles("#main-content", 8);
 
 
-function insertHTML (selector, html) {
-  var targetElem = document.querySelector(selector);
-  targetElem.innerHTML = html;
-}
+  });
+
+
+  function buildAndShowImageTiles (selector, numberOfTiles) {
+
+    $ajaxUtils.sendGetRequest("snippets/image-snippet.html", function (response)
+    {
+      var finalHTML = '<section class="row">';
+      for (var i = 0; i < numberOfTiles; i++) {
+        finalHTML += response;
+      };
+      finalHTML += '</section>';
+      insertHTML(selector, finalHTML);
+      }, false);
+  }
+
+
+  function insertHTML (selector, html) {
+    var targetElem = document.querySelector(selector);
+    targetElem.innerHTML = html;
+  }
+
+})(window);
