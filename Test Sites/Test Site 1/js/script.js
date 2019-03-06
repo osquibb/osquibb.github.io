d21df1a1eb0c3d40cf7c5edcc68cc905ae49cdb8
osquibb <osquibb@gmail.com>
2019-03-06 11:18:21 -0500

    updated json AJAX request functionality

--- Test Sites/Test Site 1/js/script.js
+++ Test Sites/Test Site 1/js/script.js
@@ -1,36 +1,13 @@
-// TODO: Create a JSON file, actually an array of json strings
-// (and get with ajax request) that populates
-// the tiles with the buildAndShowImageTiles function.
-
 (function (window) { // IIFE start
-
-
-  var image = {
-    url: "images/icon.png",
-    title: "Image Title",
-    description: "Some quick example text.",
-    link: "https://www.google.com"
-  };
-  // Dummy JSON array
-  var images = [image, image, image, image, image];
-
-  // test array ***
-  var jsonImages = []
-
 
 // .addEventListener start
   document.addEventListener("DOMContentLoaded", function (event) {
 
-    buildAndShowImageTiles("#main-content", images);
-
-    // JSON test area ***
-
+    // GET images json, then buildAndShowImageTiles
     $ajaxUtils.sendGetRequest("data/images.json", function(response) {
-      console.log(response[0]); }, true);
-
-    // JSON test area ***
-
-
+      console.log(response[1]);
+      buildAndShowImageTiles("#main-content", response);
+      }, true);
 
   }); // .addEventListener end
 
@@ -39,6 +16,8 @@
 
     $ajaxUtils.sendGetRequest("snippets/image-snippet.html", function (response)
     {
+      // assign response's original state (w/ {{}}}) to snippet
+      var snippet = response;
       var finalHTML = '<section class="row">';
       for (var i = 0; i < images.length; i++) {
         response = insertProperty(response, "image-url", images[i].url);
@@ -46,6 +25,9 @@
         response = insertProperty(response, "image-description", images[i].description);
         response = insertProperty(response, "image-link", images[i].link);
         finalHTML += response;
+        // reset response to original state so
+        // that insertProperty can work for subsequent iterations
+        response = snippet;
       };
       finalHTML += '</section>';
       insertHTML(selector, finalHTML);
